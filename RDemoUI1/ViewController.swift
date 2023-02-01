f0eb4a4baf0bbc31ca51020dfc8c186fc2ff14f3
Ihor Malovanyi <mail@ihor.pro>
2023-02-01 14:20:09 +0200

    [update] add some logic

--- RDemoUI1/ViewController.swift
+++ RDemoUI1/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    private var flowLayout: UICollectionViewFlowLayout = {
+        let result = UICollectionViewFlowLayout()
+        result.itemSize = .init(width: 150, height: 150)
+        result.scrollDirection = .horizontal
+        return result
+    }()
+    
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-
+    
+    private func setupUI() {
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.backgroundColor = .red
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            collectionView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            collectionView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            collectionView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        collectionView.reloadData()
+    }
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.contentView.backgroundColor = .black
+        return cell
+    }
+
+}
